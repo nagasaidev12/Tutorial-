6d77627e63800ed1f8c0f21a443de1959c09b7d7
nagasaidev12 <115387069+nagasaidev12@users.noreply.github.com>
2022-12-04 11:30:57 +0530

    Add files via upload

---
+++ for loop q6.py
@@ -0,0 +1,3 @@
+input_number = 6
+cube=input_number**3
+print("current number is :{0} and the cube is {1}".format(input_number,cube))
---
+++ q3.py
@@ -0,0 +1,5 @@
+list=[8,2,3,-1,7]
+m=1
+for i in list:
+    m=m*i
+print(m)
